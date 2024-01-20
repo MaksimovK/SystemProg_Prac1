84279a4b1477af50339c1c70200be6d765931bbf
Jozer <kmakismov@yandex.ru>
2024-01-20 14:45:32 +0500

    first commit

---
+++ ConsoleApp/ConsoleApp/ConsoleApp.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int countDigits(int number) {
+    int count = 0;
+    while (number != 0) {
+        number /= 10;
+        ++count;
+    }
+    return count;
+}
+
+int main() {
+    int number;
+    setlocale(LC_ALL, "rus");
+    cout << "Введите число: ";
+    cin >> number;
+
+    int digitCount = countDigits(number);
+    cout << "Количество цифр в числе: " << digitCount << std::endl;
+
+    return 0;
+}
---
+++ ConsoleApp/ConsoleApplication/ConsoleApplication.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int countDigits(int number) {
+    int count = 0;
+    while (number != 0) {
+        number /= 10;
+        ++count;
+    }
+    return count;
+}
+
+void printEvenDigits(int number) {
+    cout << "Четные цифры числа: ";
+    while (number != 0) {
+        int digit = number % 10;
+        if (digit % 2 == 0) {
+            cout << digit << " ";
+        }
+        number /= 10;
+    }
+    cout << endl;
+}
+
+int main() {
+    int number;
+    setlocale(LC_ALL, "rus");
+    cout << "Введите число: ";
+    cin >> number;
+
+    int digitCount = countDigits(number);
+
+    if (digitCount == 3) {
+        printEvenDigits(number);
+    }
+    else {
+        cout << "Число не является трехзначным." << endl;
+    }
+
+    return 0;
+}
